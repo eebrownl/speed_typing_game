428a9a375527f979849bf05cdc3c92ddbbd123bc
Erika Brownlee <eebrownl@gmail.com>
2022-11-07 06:45:42 -0700

    added logic for disabling button and textarea

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react'
 
 function App() {
+const STARTING_TIME = 2
+
   const [typedText, setTypedText] = React.useState('')
-  const [timeRemaining, setTimeRemaining] = React.useState(5)
+  const [timeRemaining, setTimeRemaining] = React.useState(STARTING_TIME)
   const [gameStarted, setGameStarted] = React.useState(false)
   const [wordCount, setWordCount] = React.useState(0)
+  
 
   function handleChange(e) {
     setTypedText(e.target.value)
@@ -18,17 +21,23 @@ function App() {
 
   function startGame() {
     setGameStarted(true)
+    setWordCount(0)
+    setTimeRemaining(STARTING_TIME)
+    setTypedText('')
   }
 
+  function endGame() {
+    setGameStarted(false)
+    const numWords = countWords(typedText)
+    setWordCount(numWords)
+  }
   useEffect(() => {
     if(timeRemaining > 0 && gameStarted) {
       setTimeout(() => {
         setTimeRemaining(time => time - 1)
     }, 1000)
     } else if(timeRemaining === 0) {
-      setGameStarted(false)
-      const numWords = countWords(typedText)
-      setWordCount(numWords)
+      endGame()
     }
 }, [timeRemaining, gameStarted])
 
@@ -38,9 +47,10 @@ function App() {
       <textarea 
         name='typing-area'
         value={typedText}
-        onChange={handleChange}/>
+        onChange={handleChange}
+        disabled = {!gameStarted}/>
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={startGame}>Start</button>
+      <button onClick={startGame} disabled = {gameStarted}>Start</button>
       <h1>Word Count:{wordCount}</h1>
     </div>
   )
