199de4461c53f2f451a4407e8d571e44c206d8d5
Erika Brownlee <eebrownl@gmail.com>
2022-11-06 21:01:31 -0700

    created wordCount state and set logic to grab count at end of game

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   const [typedText, setTypedText] = React.useState('')
   const [timeRemaining, setTimeRemaining] = React.useState(5)
   const [gameStarted, setGameStarted] = React.useState(false)
+  const [wordCount, setWordCount] = React.useState(0)
 
   function handleChange(e) {
     setTypedText(e.target.value)
@@ -12,7 +13,7 @@ function App() {
   function countWords(str) {
     const arr = typedText.trim().split(' ');
     const filteredWords = arr.filter(word => word !== '')
-    console.log(filteredWords.length) 
+    return filteredWords.length
   }
 
   function startGame() {
@@ -26,6 +27,8 @@ function App() {
     }, 1000)
     } else if(timeRemaining === 0) {
       setGameStarted(false)
+      const numWords = countWords(typedText)
+      setWordCount(numWords)
     }
 }, [timeRemaining, gameStarted])
 
@@ -38,7 +41,7 @@ function App() {
         onChange={handleChange}/>
       <h4>Time Remaining: {timeRemaining}</h4>
       <button onClick={startGame}>Start</button>
-      <h1>Word Count:</h1>
+      <h1>Word Count:{wordCount}</h1>
     </div>
   )
 }
